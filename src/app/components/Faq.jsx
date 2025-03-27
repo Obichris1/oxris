40449d8d3f4699f78c85c901defb764f25a40bfe
Macbook Air <macbookair@Obinnas-MacBook-Air.local>
2025-03-27 16:36:08 +0100

    service fix

--- src/app/components/Faq.jsx
+++ src/app/components/Faq.jsx
@@ -46,10 +46,10 @@ export default function FAQSection() {
         transition={{ duration: 0.5 }}
         className="text-center mb-8"
       >
-        <Typography variant="h4" className="!font-bold !text-2xl md:!text-3xl">
+        <Typography variant="h4" className="!font-bold !text-2xl !mb-4 md:!text-3xl">
           Frequently Asked Questions
         </Typography>
-        <Typography className="text-gray-500 mt-4 text-sm md:text-base ">
+        <Typography className="text-gray-500  text-sm md:text-base ">
           Here are some of the common questions we get from our clients.
         </Typography>
       </motion.div>
