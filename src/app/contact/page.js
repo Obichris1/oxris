d79597baf49e2c7812e59111e66f855bf571fc32
Macbook Air <macbookair@Obinnas-MacBook-Air.local>
2025-06-20 11:55:57 +0100

    redirect fix

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -22,13 +22,14 @@ const Page = () => {
         transition={{ duration: 0.8, ease: "easeOut" }}
         className="flex flex-col gap-4 w-full md:w-[50%]"
       >
-        <Typography variant="h4" className="!font-semibold !text-2xl md:!text-3xl !text-center md:!text-left">
+        <Typography
+          variant="h4"
+          className="!font-semibold !text-2xl md:!text-3xl !text-center md:!text-left"
+        >
           REACH OUT TO US
         </Typography>
 
         <hr />
-
-       
 
         <div className="text-[#aaa5a5] text-xs md:text-sm flex flex-col gap-3">
           {/* <Typography className="font-bold uppercase text-black mb-4">
@@ -37,47 +38,51 @@ const Page = () => {
 
           <span className="flex items-center gap-4 text-gray-500">
             <AiOutlineMail className="text-lg" />
-            <Link href="mailto:obichris202@gmail.com?subject=Inquiry&body=Hello, I want to make enquiries..." className="hover:text-primary">
-             obichris202@gmail.com
+            <Link
+              href="mailto:obichris202@gmail.com?subject=Inquiry&body=Hello, I want to make enquiries..."
+              className="hover:text-primary"
+            >
+              obichris202@gmail.com
             </Link>
           </span>
           <span className="flex items-center gap-4 text-gray-500 ">
             <AiOutlinePhone className="text-lg" />
-            <Link href="tel:+2348129614462" className="hover:text-primary">+234 812 961 4462</Link>,
-            <Link href="tel:+2349150831932" className="hover:text-primary">+234 915 083 1932</Link>
-           
+            <Link href="tel:+2348129614462" className="hover:text-primary">
+              +234 812 961 4462
+            </Link>
+            ,
+            <Link href="tel:+2349150831932" className="hover:text-primary">
+              +234 915 083 1932
+            </Link>
           </span>
         </div>
 
-          {/* SOCIAL MEDIA  */}
-          <div className="flex  md:self-end  items-center gap-4 mt-3">
+        {/* SOCIAL MEDIA  */}
+        <div className="flex  md:self-end  items-center gap-4 mt-3">
+          <Link
+            href=""
+            target="_blank"
+            className="p-3 bg-[#E12F6C] rounded-full text-white hover:scale-75 transition"
+          >
+            <AiOutlineInstagram />
+          </Link>
 
+          <Link
+            href=""
+            target="_blank"
+            className="p-3 bg-[#3C5997] rounded-full text-white hover:scale-75 transition"
+          >
+            <AiFillFacebook />
+          </Link>
 
-<Link
-  href=""
-  target="_blank"
-  className="p-3 bg-[#E12F6C] rounded-full text-white hover:scale-75 transition"
->
-  <AiOutlineInstagram />
-</Link>
-
-<Link
-  href=""
-  target="_blank"
-  className="p-3 bg-[#3C5997] rounded-full text-white hover:scale-75 transition"
->
-  <AiFillFacebook />
-</Link>
-
-<Link
-  href=""
-  target="_blank"
-  className="p-3 bg-[#000] rounded-full text-white hover:scale-75 transition"
->
-  <AiOutlineX />
-</Link>
-</div>
-
+          <Link
+            href=""
+            target="_blank"
+            className="p-3 bg-[#000] rounded-full text-white hover:scale-75 transition"
+          >
+            <AiOutlineX />
+          </Link>
+        </div>
       </motion.div>
 
       {/* Contact Form - Slides in from the right */}
